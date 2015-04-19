4b49ab0f0d08f51d7eeec1ea9e9477e4d19e83ee
three-seven@yandex.ru <three-seven@yandex.ru>
2015-04-19 15:33:35 +0600

    Class Product added.

---
+++ App_Code/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+/// <summary>
+/// Summary description for Product
+/// </summary>
+public class Product
+{
+    public Product()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public int ProductId { get; set; }
+    [StringLength(50)]
+    public string Name { get; set; }
+    [StringLength(1000)]
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    [StringLength(50)]
+    public string ImageName { get; set; }
+}
