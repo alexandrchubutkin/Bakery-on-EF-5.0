efa281385f17c7c139a90bef477f9dd5948cdc7b
three-seven@yandex.ru <three-seven@yandex.ru>
2015-04-19 13:42:57 +0600

    BakeryDbContext class added.

---
+++ App_Code/BakeryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+
+/// <summary>
+/// Summary description for BakeryDbContext
+/// </summary>
+public class BakeryDbContext : DbContext
+{
+    public BakeryDbContext()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        this.Configuration.LazyLoadingEnabled = false;
+    }
+}
