ebeba8fe0f002f08eca67afe1a71077ef10b459e
three-seven@yandex.ru <three-seven@yandex.ru>
2015-04-19 15:56:02 +0600

    Correct views, change initializers and run.

--- App_Code/BakeryDbContext.cs
+++ App_Code/BakeryDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Data.Entity;
 using System.Data.Entity.Validation;
+using System.Data.Entity.Migrations;
 
 /// <summary>
 /// Summary description for BakeryDbContext
@@ -14,9 +16,37 @@ public class BakeryDbContext : DbContext
         //
         // TODO: Add constructor logic here
         //
-        Configuration.LazyLoadingEnabled = false;
-        Database.SetInitializer<BakeryDbContext>(new BakeryDbContextInitializer<BakeryDbContext>());
+        Configuration.LazyLoadingEnabled = false;         
     }
 
     public DbSet<Product> Products { get; set; }
+}
+
+// Migrations 
+/*
+    In _AppStart.cshtml create string:
+    System.Data.Entity.Database.SetInitializer<BakeryContext>(new BakeryContextInitializer());
+*/
+public class BakeryDbContextInitializer : MigrateDatabaseToLatestVersion<BakeryDbContext, BakeryDbContextDbMigrationsConfiguration> 
+{
+
+}
+
+public sealed class BakeryDbContextDbMigrationsConfiguration : DbMigrationsConfiguration<BakeryDbContext> 
+{
+    public BakeryDbContextDbMigrationsConfiguration() 
+    {
+        AutomaticMigrationsEnabled = true;
+        AutomaticMigrationDataLossAllowed = true;
+    }
+
+    protected override void Seed(BakeryDbContext context)
+    {
+        var products = WebMatrix.Data.Database.Open("bakery").Query("select * from Products").ToList();
+        foreach (var p in products) {
+            context.Products.Add(new Product() { Name = p.Name, Description = p.Description, 
+                Price = p.Price, ImageName = p.ImageName });
+        }
+        base.Seed(context);
+    }
 }
