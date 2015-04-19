4b3051c29f0a08b8d6cf96774458cf0d074da907
three-seven@yandex.ru <three-seven@yandex.ru>
2015-04-19 15:41:21 +0600

    Create initializer for BakeryDbContext (with migrate data from old database)

---
+++ App_Code/BakeryDbContextInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+public class BakeryDbContextInitializer : DropCreateDatabaseAlways<BakeryDbContext>
+{
+    protected override void Seed(BakeryDbContext context)
+    {
+        base.Seed(context);
+    }
+}
